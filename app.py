808670b8e1f6a3da80a2b750e8f394f547cbb17d
paliandris02 <pali.andris@gmail.com>
2023-08-14 22:28:12 +0200

    req

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
-from revel.main import Estimate
+from src.revel.main import Estimate
 from flask_cors import CORS
 
 app: Flask = Flask(__name__)
 
 allowed_origins = ['http://localhost:8080']
 CORS(app, resources={r"/estimate": {"origins": allowed_origins}})
+
+@app.route("/")
+def index():
+    return "default page"
 
 @app.route('/estimate', methods=['POST'])
 def estimate():
